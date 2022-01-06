ae5a106a47489255f44569f4d871a72f6672f8c8
Maksim_Raspopov <hardlolmaster@bk.ru>
2022-01-06 12:06:02 +0500

    user task

---
+++ CamundaBpmn/Tasks/UserTask/src/main/java/com/gihub/hlam/MainUserTask.java
@@ -0,0 +1,37 @@
+package com.gihub.hlam;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.task.Task;
+
+public class MainUserTask implements JavaDelegate {
+    public static void main(String[] args) {
+        ProcessEngine processEngine = ProcessEngineConfiguration
+            .createStandaloneInMemProcessEngineConfiguration()
+            .buildProcessEngine();
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.createDeployment()
+            .addClasspathResource("user.bpmn")
+            .deploy();
+
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        runtimeService.startProcessInstanceByKey("user");
+
+        TaskService taskService = processEngine.getTaskService();
+        Task task = taskService.createTaskQuery().singleResult();
+        taskService.complete(task.getId());
+
+        processEngine.close();
+    }
+
+    @Override
+    public void execute(DelegateExecution execution) {
+        System.out.println(getClass());
+    }
+}
